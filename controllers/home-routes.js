cc45879c620da952ab4b7f8f0acb1aa447eb2642
Kyle Reynolds <kyle.reynolds9146@gmail.com>
2020-11-14 15:19:56 -0800

    added photo uploading and post model

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -6,6 +6,7 @@ cloudinary.config({ cloud_name: 'actively-distanced', api_key: '459732884598213'
 const dataURI = require('datauri');
 const dURI = new dataURI();
 const path = require('path');
+const Post = require('../models/Post');
 
 router.get('/', (req, res) => {
     res.render('homepage');
@@ -15,12 +16,23 @@ router.post('/profile', upload.single('photo'), function (req, res, next) {
     // req.file is the `avatar` file
     // req.body will hold the text fields, if there were any
     // let data = dataURI.format(path.extname(req.file.originalname).toString(), req.file.buffer);
+    console.log(req.body);
     return cloudinary.uploader.upload(req.file.path)
         .then((result) => {
-            return res.status(200).json({
-                data: result.url
+            Post.create({
+                title: req.body.title,
+                description: req.body.description,
+                date: req.body.date,
+                time: req.body.time,
+                image_url: result.url
             })
+
+            // return res.status(200).json({
+            //     data: result.url
+            // })
+            return res.redirect('/events')
         })
+        .catch((err) => console.log(err))
 });
 
 module.exports = router;
