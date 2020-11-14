af7c73b3e8673ce493b19d0b16ef74e8a66a9166
Kyle Reynolds <kyle.reynolds9146@gmail.com>
2020-11-14 14:50:29 -0800

    added cloudinary and multer

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,6 +1,11 @@
 const router = require('express').Router();
 const multer = require('multer');
 const upload = multer({ dest: 'uploads/' });
+const cloudinary = require('cloudinary').v2;
+cloudinary.config({ cloud_name: 'actively-distanced', api_key: '459732884598213', api_secret: '69tQZU3yr0mFsxuNe2U2WCDR544' });
+const dataURI = require('datauri');
+const dURI = new dataURI();
+const path = require('path');
 
 router.get('/', (req, res) => {
     res.render('homepage');
@@ -9,6 +14,13 @@ router.get('/', (req, res) => {
 router.post('/profile', upload.single('photo'), function (req, res, next) {
     // req.file is the `avatar` file
     // req.body will hold the text fields, if there were any
-})
+    // let data = dataURI.format(path.extname(req.file.originalname).toString(), req.file.buffer);
+    return cloudinary.uploader.upload(req.file.path)
+        .then((result) => {
+            return res.status(200).json({
+                data: result.url
+            })
+        })
+});
 
 module.exports = router;
