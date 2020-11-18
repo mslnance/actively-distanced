b6f6bad1e73db5da2e5828a13166009538067be4
Kyle Reynolds <kyle.reynolds9146@gmail.com>
2020-11-17 17:15:22 -0800

    merged with shilu's branch

--- models/User.js
+++ models/User.js
@@ -5,9 +5,9 @@ const sequelize = require('../config/connection');
 // create our User model
 class User extends Model {
     // set up method to run on instance data (per user) to check password
-    checkPassword(loginPw) {
-        return bcrypt.compareSync(loginPw, this.password);
-    }
+    // checkPassword(loginPw) {
+    //     return bcrypt.compareSync(loginPw, this.password);
+    // }
 }
 
 // create fields/columns for User model
@@ -22,14 +22,6 @@ User.init(
         username: {
             type: DataTypes.STRING,
             allowNull: false
-        },
-        email: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            unique: true,
-            validate: {
-                isEmail: true
-            }
         },
         password: {
             type: DataTypes.STRING,
