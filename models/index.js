875179b2f58d03163a4edb009f2d36abf9f4d674
Kyle Reynolds <kyle.reynolds9146@gmail.com>
2020-11-19 19:16:22 -0800

    adding comment changes

--- models/index.js
+++ models/index.js
@@ -10,7 +10,6 @@ User.hasMany(Post, {
 
 Post.belongsTo(User, {
     foreignKey: 'user_id',
-    // onDelete: 'SET NULL'
 });
 
 Comment.belongsTo(User, {
--- public/javascript/comment.js
+++ public/javascript/comment.js
@@ -2,13 +2,14 @@ async function commentFormHandler(event) {
     event.preventDefault();
 
     const comment_text = document.querySelector('textarea[name="comment-body"]').value.trim();
+    console.log(`post id: ${post_id}`);
+    console.log(`comment text: ${comment_text}`);
+
     const post_id = window.location.toString().split('/')[
         window.location.toString().split('/').length - 1
     ];
 
     if (comment_text) {
-        console.log(`post id: ${post_id}`);
-        console.log(`comment text: ${comment_text}`);
 
         const response = await fetch('/api/comments', {
             method: 'POST',
