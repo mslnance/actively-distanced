7feb96c0bdb38e26e6badbc1299288ac2c295b85
Kyle Reynolds <kyle.reynolds9146@gmail.com>
2020-11-23 19:18:18 -0800

    updated comments

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -12,9 +12,6 @@ router.get('/', (req, res) => {
 });
 
 router.post('/', withAuth, (req, res) => {
-    console.log(req.body.comment_text);
-    console.log(req.body.activity_id);
-
     Comment.create({
         comment_text: req.body.comment_text,
         user_id: req.session.user_id,
